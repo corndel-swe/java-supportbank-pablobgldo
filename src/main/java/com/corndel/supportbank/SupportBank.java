dd53c59c85c50538a4ab279710349a9dc2b7af6c
pablobgldo <pablobgldo@gmail.com>
2024-09-30 10:16:13 +0100

    Added Chalk dependency

--- src/main/java/com/corndel/supportbank/SupportBank.java
+++ src/main/java/com/corndel/supportbank/SupportBank.java
@@ -1,8 +1,11 @@
 package com.corndel.supportbank;
 
+import com.github.tomaslanger.chalk.Chalk;
+
 public class SupportBank {
 
   public static void main(String[] args) {
-    System.out.println("Build your CLI here!");
+
+    System.out.println(Chalk.on("Build your CLI here!").red());
   }
 }
