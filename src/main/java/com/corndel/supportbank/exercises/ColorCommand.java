7ac9fe9707935f6ca91f6d5db7285bfbe72fc71a
pablobgldo <pablobgldo@gmail.com>
2024-09-30 11:13:30 +0100

    Completed exercises 1-4

--- src/main/java/com/corndel/supportbank/exercises/ColorCommand.java
+++ src/main/java/com/corndel/supportbank/exercises/ColorCommand.java
@@ -1,18 +1,18 @@
-// package com.corndel.supportbank.exercises;
+ package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine.Command;
+ import picocli.CommandLine.Command;
 
-// @Command(name = "color", description = "What color should you wear today?")
-// public class ColorCommand implements Runnable {
-// /**
-// * Print out a randomly chosen color to wear today.
-// */
-// @Override
-// public void run() {
-// String[] colors = { "red", "green", "blue", "yellow" };
-// String color = colors[(int) (Math.random() * colors.length)];
+ @Command(name = "color", description = "What color should you wear today?")
+ public class ColorCommand implements Runnable {
+ /**
+ * Print out a randomly chosen color to wear today.
+ */
+ @Override
+ public void run() {
+ String[] colors = { "red", "green", "blue", "yellow" };
+ String color = colors[(int) (Math.random() * colors.length)];
 
-// String msg = String.format("Today you should wear %s!", color);
-// System.out.println(msg);
-// }
-// }
+ String msg = String.format("Today you should wear %s!", color);
+ System.out.println(msg);
+ }
+ }
--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,25 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
-// import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello", subcommands = ColorCommand.class)
+public class HelloWorld implements Runnable {
+
+    @Parameters(index = "0", defaultValue = "World")
+    private String name;
+
+    @Override
+    public void run() {
+        String msg = String.format("Hello, %s!", name);
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        CommandLine cli = new CommandLine(new HelloWorld());
+        int exitCode = cli.execute(args);
+        System.exit(exitCode);
+    }
+
 }
