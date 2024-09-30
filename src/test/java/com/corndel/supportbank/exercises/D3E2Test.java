a7661bf778419994d354fefb48f01f2c0107334f
Simon Haines <nimon@hotmail.co.uk>
2024-09-30 01:00:29 +0100

    let it throw!

--- src/test/java/com/corndel/supportbank/exercises/D3E2Test.java
+++ src/test/java/com/corndel/supportbank/exercises/D3E2Test.java
@@ -6,14 +6,10 @@ import org.junit.jupiter.api.Test;
 
 public class D3E2Test {
   @Test
-  public void testPokemonAPI() {
-    try {
+  public void testPokemonAPI() throws Exception {
       Pokemon pokemon = PokeAPI.getPokemonByName("pikachu");
       assertEquals("pikachu", pokemon.name().trim().toLowerCase());
       assertEquals(60, pokemon.weight());
       assertEquals(25, pokemon.id());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
