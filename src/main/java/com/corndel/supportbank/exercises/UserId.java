aca50fa9bd205201469c959e72d3fe3a94f7114b
pablobgldo <pablobgldo@gmail.com>
2024-10-01 11:49:58 +0100

    Tidied up exercises

--- src/main/java/com/corndel/supportbank/exercises/UserId.java
+++ src/main/java/com/corndel/supportbank/exercises/UserId.java
@@ -8,39 +8,20 @@ public class UserId {
     this.id = id;
   }
 
-  /**
-   * Validates the user ID based on specific criteria.
-   *
-   * //@param id The user ID to validate.
-   * @return true if the ID passes all validations.
-   * @throws IllegalArgumentException if any validation fails.
-   */
   public boolean validate() {
-    // TODO: this.id must be an odd number of characters long
-    // If not, throw an IllegalArgumentException with the message
-    // "The id must be an odd number of characters long"
 
     if (this.id.length() % 2 == 0) {
       throw new IllegalArgumentException("The id must be an odd number of characters long");
     }
 
-    // TODO: this.id must contain the letter 'a'
-    // If not, throw an IllegalArgumentException with the message
-    // "The id must contain the letter 'a'
-
     if (!this.id.contains("a")) {
       throw new IllegalArgumentException("The id must contain the letter 'a'");
     }
-
-    // TODO: this.id must be all lowercase:
-    // If not, throw an IllegalArgumentException with the message
-    // "The id must be all lowercase"
 
     if (!this.id.toLowerCase().equals(this.id)) {
       throw new IllegalArgumentException("The id must be all lowercase");
     }
 
-    // If all validations pass, return true.
     return true;
   }
 }
