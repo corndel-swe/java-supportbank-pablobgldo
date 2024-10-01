110bbd0011fae720e028de83d9226b769b017586
pablobgldo <pablobgldo@gmail.com>
2024-10-01 14:05:50 +0100

    Tidied up Element

--- src/main/java/com/corndel/supportbank/exercises/Element.java
+++ src/main/java/com/corndel/supportbank/exercises/Element.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.nio.file.*;
-import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
