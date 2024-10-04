e953e4e8424d015deef06fe2114dfd1d3c39b8ec
pablobgldo <pablobgldo@gmail.com>
2024-10-04 12:03:35 +0100

    Completed Day 3 Extension exercise

--- src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
+++ src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
@@ -1,9 +1,16 @@
 package com.corndel.supportbank.exercises;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import kong.unirest.Unirest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This class represents a Pokemon. It uses Java's record syntax to
@@ -44,6 +51,42 @@ public class PokeAPI {
     return pokemon;
   }
 
+  public static List<Pokemon> getPokemonList() throws Exception {
+      String url = "https://pokeapi.co/api/v2/pokemon/";
+      var response = Unirest
+            .get(url)
+            .header("Content-Type", "application/json")
+            .asString();
+    String json = response.getBody();
+    ObjectMapper mapper = new ObjectMapper();
+    var rootNode = mapper.readTree(json);
+    var results = rootNode.get("results");
+    List<Map<String, String>> list = mapper.convertValue(results, new TypeReference<>() {});
+
+    List<String> names = list.stream().map(x -> x.get("name")).toList();
+
+    List<Pokemon> pokemonList = new ArrayList<>();
+
+    for (String name : names) {
+      String url2 = "https://pokeapi.co/api/v2/pokemon/" + name;
+      var response2 = Unirest
+              .get(url2)
+              .header("Content-Type", "application/json")
+              .asString();
+      String json2 = response2.getBody();
+      ObjectMapper mapper2 = new ObjectMapper();
+      var rootNode2 = mapper2.readTree(json2);
+      int id = rootNode2.get("id").asInt();
+      int height = rootNode2.get("height").asInt();
+      int weight = rootNode2.get("weight").asInt();
+
+      Pokemon pokemon = new Pokemon(id, name, height, weight);
+      pokemonList.add(pokemon);
+    }
+    System.out.println(pokemonList);
+      return pokemonList;
+  }
+
   /**
    * For debugging purposes..
    */
@@ -51,6 +94,7 @@ public class PokeAPI {
     try {
       Pokemon pokemon = getPokemonByName("pikachu");
       System.out.println(pokemon);
+      getPokemonList();
     } catch (Exception e) {
       e.printStackTrace();
     }
